33ffd22bd0bf6df4b447612badfd6bd50a8b3412
FloWambui <gflorencewambui@gmail.com>
2022-03-14 01:15:57 +0300

    feat & style:add toggle functionality to the portfolio page

---
+++ js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    $(".design").click(function(){
+        $(".imgdesign").toggle();
+        $(".description1").toggle();
+    });
+    $(".development").click(function(){
+        $(".imgdevelopment").toggle();
+        $(".description2").toggle();
+    });
+    $(".productmngt").click(function(){
+        $(".productmngtimg").toggle();
+        $(".description3").toggle();
+    });
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function submitmsg(){
+//     var name=document.getElementById("name").value;
+//     var email=document.getElementById("email").value;
+//     var message=document.getElementById("message").value;
+//     if ((name=== false) && (email === false) && (message === false)){
+//         alert("please fill in all fields")
+//         document.getElementById("form")
+
+//     }
+    
+  
+// }
